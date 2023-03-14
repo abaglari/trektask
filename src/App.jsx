e42dd960dd0630876d5ff8daa72fa851a8d56fc6
abaglari <127620023+abaglari@users.noreply.github.com>
2023-03-14 22:52:54 +0530

    Added Routes and Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SiteLayout from "./components/SiteLayout";
+import Home from "./routes/Site Routes/Home";
+import Features from "./routes/Site Routes/Features";
+import Contact from "./routes/Site Routes/Contact";
+import AppLayout from "./components/AppLayout";
+import AuthContextProvider from "./context/AuthContext";
+import Signin from "./routes/Site Routes/Signin";
+import Signup from "./routes/Site Routes/Signup";
+import Protected from "./routes/Site Routes/Protected";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Router>
+        <AuthContextProvider>
+          <Routes>
+            <Route path='/' element={<SiteLayout />}>
+              <Route index element={<Home />} />
+              <Route path='/features' element={<Features />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='/signin' element={<Signin />} />
+              <Route path='/signup' element={<Signup />} />
+            </Route>
+            <Route
+              path='/app'
+              element={
+                <Protected>
+                  <AppLayout />
+                </Protected>
+              }
+            />
+          </Routes>
+        </AuthContextProvider>
+      </Router>
+    </>
+  );
 }
 
 export default App;
