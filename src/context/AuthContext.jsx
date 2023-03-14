f87dbc491c549c59cd4f0dca76b8b5561b16b844
abaglari <127620023+abaglari@users.noreply.github.com>
2023-03-14 22:48:47 +0530

    Updated Authentication logic

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,123 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  onAuthStateChanged,
+  updateProfile,
+  sendEmailVerification,
+} from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState();
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const provider = new GoogleAuthProvider();
+
+  //Sign up with Email and Password
+  const signup = async (email, password, name) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await updateProfile(userCredential.user, {
+        displayName: name,
+      });
+      await sendEmailVerification(userCredential.user);
+      setError("Verification mail has been sent to your email");
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        setError("Email is already registered");
+      } else if (error.code === "auth/invalid-email") {
+        setError("Invalid email address");
+      } else if (error.code === "auth/weak-password") {
+        setError("Password is too weak");
+      } else {
+        setError("Failed to sign up. Please try again later.");
+      }
+    }
+  };
+
+  //Sign in with Email and Password
+  const signin = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (!userCredential.user.emailVerified) {
+        await sendEmailVerification(userCredential.user);
+        setError("Please verify your email address to login.");
+      }
+    } catch (error) {
+      if (error.code === "auth/wrong-password") {
+        setError("Incorrect Password");
+      } else if (error.code === "auth/user-not-found") {
+        setError("Invalid User");
+      } else if (error.code === "auth/invalid-email") {
+        setError("Invalid Email");
+      } else {
+        console.log(error.code);
+        setError("Failed to log in. Please try again later.");
+      }
+    }
+  };
+
+  //Google Sign in
+  const googleSignIn = () => {
+    return signInWithRedirect(auth, provider);
+  };
+
+  //Sign out
+  const signout = () => {
+    signOut(auth);
+  };
+
+  //Set user onAuthStateChanged
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // Check if email is verfied if not sign out the user, if verfied navigate to the app
+      if (currentUser && !currentUser.emailVerified) {
+        signOut(auth);
+        console.log("signed out");
+      } else {
+        setUser(currentUser);
+        navigate("/app");
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signin,
+        signout,
+        googleSignIn,
+        signup,
+        error,
+        setError,
+      }}
+    >
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
+
+export default AuthContextProvider;
